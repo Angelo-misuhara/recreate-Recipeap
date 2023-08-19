d140cc30ab45eb969eeaee9f29d70a4ee6717767
ogel <yocorangelo18@gmail.com>
2023-08-19 14:23:29 +0800

    fix bbbbugs more bugs potangna

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <div className="flex justify-center">
           <ul className="flex flex-row gap-5 justify-center items-center">
             {li.map((item, key) => (
-              <li key={key} className="font-bold cursor-pointer hover:text-[#f0c60a]">{item}</li>
+              <li key={key} className="font-bold cursor-pointer hover:text-[#65955f]">{item}</li>
             ))}
           </ul>
         </div>
@@ -25,14 +25,14 @@ const Navbar = () => {
       <div className=" hidden flex-row m-4 justify-between" id="showcp">
         <div className="flex flex-row gap-2">
           <img src="/img/icon64.png" alt="icon" className="w-1/6 hover:animate-spin" />
-          <h1 className="text-2xl font-black">Cookingina</h1>
+          <h1 className="text-2xl font-black" id="cookinginaphone">Cookingina</h1>
         </div>
         <div>
           <img src={isShow ? '/img/close32.png' : '/img/menu32.png'} alt="icon" onClick={()=> setShow(!isShow)} />
         </div>
       </div>
-      {isShow && <div className="p-4">
-        <ul className="flex flex-col gap-2">
+      {isShow && <div className="p-5">
+        <ul className="flex flex-col gap-4">
           {li.map((item, key) => (
             <li key={key} className="font-bold cursor-pointer hover:text-[#f0c60a]">{item}</li>
          ))}
--- src/layout/Searchbar.jsx
+++ src/layout/Searchbar.jsx
@@ -1,20 +1,31 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Searchbar = () => {
-  const [search, setSearch] = useState('adobo');
+  const [search, setSearch] = useState('');
   const [data, setData] = useState([]);
+  const [strMealThumb, setStrMealThumb] = useState('')
+  const [strYoutube, setStrYoutube] = useState('')
+  const [strSource, setStrSource] = useState('')
+  const [strMeal, setStrMeal] = useState('')
+  const [notFound , setNotfound] = useState(false)
 
  const fetchData =() => {
    if (search !== '') {
       axios.get(`https://www.themealdb.com/api/json/v2/1/search.php?s=${search}`)
-      .then((response) => {
-          setData(response.data.meals)
+        .then((response) => {
+          if (response.status === 200 && response.data.meals && response.data.meals.length > 0) {
+            setStrMealThumb(response.data.meals[0].strMealThumb)
+            setStrYoutube(response.data.meals[0].strYoutube)
+            setStrSource(response.data.meals[0].strSource)
+            setStrMeal(response.data.meals[0].strMeal)
+            setNotfound(false)
+          } else {
+             setNotfound(true)
+          }
+     
       })
    }
-   else {
-     alert('Enter a Food')
-     }
   }
 
     const handleFormSubmit = (e) => {
@@ -26,11 +37,15 @@ const Searchbar = () => {
       fetchData();
     }, []);
   
-   console.log(data)
+  console.log(notFound)
+  console.log(strMealThumb)
+  console.log(strYoutube)
+  console.log(strSource)
+    console.log(strMeal)
   return (
     <>
-    <div className=" flex flex-col justify-center">
-      <h1 className=' text-yellow-500 text-center flex justify-center pt-[4rem] p-9 text-6xl font-black'>
+    <div className=" flex flex-col justify-center bg-[#fdfcfd]">
+      <h1 className=' text-[#65955f] text-center flex justify-center pt-[4rem] p-9 text-6xl font-black'>
         Delightful Dishes, Effortlessly Discovered
       </h1>
       <div className="flex justify-center items-center px-8 pt-7" id='idForm'>
@@ -44,31 +59,21 @@ const Searchbar = () => {
                     </svg>
                 </div>
                 <input  value={search}  onChange={(e) => setSearch(e.target.value)} type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Recipe, Food ..."/>
-                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-[#65955f] hover:bg-[#395436] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
             </form>
     </div>
       </div>
       </div>
-        <div className='flex justify-center mt-10 px-9'>
-        {Array.isArray(data) ? data.map((item, key) => (
-            <div key={key} className='flex gap-9 justify-center' id='cpviewSearch'>
-                 <img src={item.strMealThumb} className='w-2/6 rounded-md' id='cpviewSearchimg' alt="" />
-            <div className='flex flex-col gap-3'>
-              <h1 className='pr-[8rem] text-2xl font-bold underline underline-offset-8' id='tittlecp'>
-                <a href={item.strYoutube} target="_blank" rel="noopener noreferrer">{item.strMeal}</a>
-              </h1>
-              
-           </div>
-            </div>
-
-      )) : <div>
-      <h1 className='text-6xl text-center' id='noFound'>Recipe Not Found</h1>
-        </div>}
+        {notFound ? <h1 className='pt-7 text-center font-black text-2xl'>No Meal Found</h1> :     <div className='flex bg-[#fdfcfd] flex-row justify-center mt-10 gap-9' id='container-Result'>
+          <img src={strMealThumb} className='w-1/6 rounded-md self-center' alt="" />
+        <div className='flex flex-col gap-3 'id='aboutResult'>
+          <h1 className='text-2xl font-black underline underline-offset-4'><a href={strSource} target='_black'>{strMeal.toUpperCase()}</a></h1>
+          {strMealThumb && <button type="button" className="focus:outline-none text-white bg-[#65955f] hover:bg-[#395436] focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"><a target='_blank'rel='noreferrer' href={strYoutube}>Watch Tutorial</a></button> }
         </div>
+      </div>}
+      
     </>
   )
 }
-//  <img src={item.strMealThumb} className='w-2/6 rounded-md' alt="" />
-    // <h1 className='text-1xl font-bold p-2'>Meal: {item.strMeal}</h1>
 export default Searchbar
